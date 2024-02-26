bb531fb1b4fb2b665207ad4334508fdea06e20e6
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2024-02-26 13:18:56 +0530

    Update Vector Store Module

--- WebService/main.py
+++ WebService/main.py
@@ -1,7 +1,21 @@
 from constants import DRIVE_FOLDER_ID
 from drive_utils import load_data_from_drive_folder
+from llama_index.core.node_parser import SentenceSplitter
 
-documents = load_data_from_drive_folder(DRIVE_FOLDER_ID)
+from vector_store import ChromaVectorStoreIndex
+from constants import VECTOR_STORE_PATH 
 
 
-print(documents)
+if __name__ == '__main__':
+    documents = load_data_from_drive_folder(DRIVE_FOLDER_ID)    
+    node_parser = SentenceSplitter(chunk_size=1024, chunk_overlap=20)
+
+    nodes = node_parser.get_nodes_from_documents(
+        documents, show_progress=False
+    )
+    # print(nodes)
+    chroma_index = ChromaVectorStoreIndex(nodes,persist_dir=VECTOR_STORE_PATH, collection='Set1')
+    index = chroma_index.get_chroma_index()
+    # print("****************************************************",index)
+    index2 = chroma_index.load_vector_store(persist_dir=VECTOR_STORE_PATH, collection_name ='Set1')
+    print(index2)
--- WebService/vector_store.py
+++ WebService/vector_store.py
@@ -1,48 +1,67 @@
 import chromadb
-from llama_index.vector_stores.chroma import ChromaVectorStore, BaseNode
-from llama_index.embeddings import BaseEmbedding
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core.schema import BaseNode
+from llama_index.core.embeddings import BaseEmbedding
 from llama_index.core import StorageContext, VectorStoreIndex 
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from constants import EMBED_MODEL
-
+from llama_index.core.llms.llm import LLM
 from typing import Union,Sequence,Optional
 from pathlib import Path 
 
 
-class ChromaVectorStoreIndex(VectorStoreIndex):
+class ChromaVectorStoreIndex(object):
     '''This class is a simple control tool for ChromaDB 
     '''
     
-    def __init__(self,nodes:Sequence[BaseNode],persist_dir : Union[Path,str],collection:str='default',**kwargs):          
-        persist_dir = persist_dir if type(persist_dir)==str else persist_dir.resolve().__str__()
-        self.chroma_client = chromadb.PersistentClient(path=persist_dir)
-        self.chroma_collection = self.client.get_or_create_collection(collection)
-        self.chroma_vector_store = ChromaVectorStore(chroma_collection=self.collection)    
-        if kwargs['embed_model'] is None :
+    def __init__(self,nodes:Sequence[BaseNode],persist_dir : Union[Path,str], 
+                collection:str='default', llm: Optional[Union[str, LLM]] = None,
+                 **kwargs):    
+
+        _persist_dir:str = persist_dir if type(persist_dir)==str else persist_dir.resolve().__str__()
+        self.chroma_client = chromadb.PersistentClient(path=_persist_dir)
+        self.chroma_collection = self.chroma_client.get_or_create_collection(collection)
+        self.chroma_vector_store = ChromaVectorStore(chroma_collection=self.chroma_collection)   
+        self.llm = llm 
+        if 'embed_model' not in kwargs :
             kwargs['embed_model'] = EMBED_MODEL
         kwargs['nodes']=nodes
         kwargs['storage_context']=StorageContext.from_defaults(
                 vector_store=self.chroma_vector_store) 
+        print(_persist_dir)
+        self.__index = VectorStoreIndex(**kwargs)
+    def get_chroma_index(self):
+        return self.__index
+    
+    def load_vector_store(self, persist_dir:Union[Path,str], 
+                            collection_name:str,embed_model:Optional[BaseEmbedding]=None) :
+        print("Create create_index_from_documents call Chroma Vector Store")
         
-        super(VectorStoreIndex, self).__init__(**kwargs)
-        
-    @classmethod   
-    def load_vector_store(persist_dir:Union[Path,str], collection_name:str,embed_model:Optional[BaseEmbedding]=None) :
-        persist_dir = persist_dir if type(persist_dir)==str else persist_dir.resolve().__str__()        
-        client = chromadb.PersistentClient(path=persist_dir)
+        _persist_dir:str = persist_dir if type(persist_dir)==str else persist_dir.resolve().__str__()        
+        client = chromadb.PersistentClient(path=_persist_dir)
         chroma_collection = client.get_or_create_collection(collection_name)
         vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
         if not embed_model:
             embed_model = EMBED_MODEL
-        index = VectorStoreIndex.from_vector_store(
+        index =VectorStoreIndex.from_vector_store(
             vector_store,
             embed_model=embed_model)
-        return index 
-
+        self.__index = index
+        
+    def get_retriever(self, llm: Optional[Union[str, LLM]] = None):
+        if not llm: llm = self.llm
+        return self.__index.as_retriever(llm = llm)
+    
+    def get_query_engine (self, llm: Optional[Union[str, LLM]] = None):
+        if not llm: llm = self.llm
+        return self.__index.as_query_engine(llm = llm)
+        
+    def get_chat_engine (self, llm: Optional[Union[str, LLM]] = None):
+        if not llm: llm = self.llm
+        return self.__index.as_chat_engine(llm = llm)
+        
+    
 # lOADING TEXT AFTER SELECT 
-
-
-
 
 
 
